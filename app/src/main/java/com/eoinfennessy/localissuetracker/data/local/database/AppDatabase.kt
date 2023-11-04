6004616dc12b75b4552ea87530cdcfbe9ce591a9
eoinfennessy <fennessyeoin@gmail.com>
2023-11-04 12:25:47 +0000

    Add Date-Long TypeConverters

--- app/src/main/java/com/eoinfennessy/localissuetracker/data/local/database/AppDatabase.kt
+++ app/src/main/java/com/eoinfennessy/localissuetracker/data/local/database/AppDatabase.kt
@@ -2,8 +2,24 @@ package com.eoinfennessy.localissuetracker.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import java.util.Date
 
 @Database(entities = [Issue::class], version = 1)
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun issueDao(): IssueDao
+}
+
+class Converters {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return value?.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? {
+        return date?.time
+    }
 }
